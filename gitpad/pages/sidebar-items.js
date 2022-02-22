3b67073a0301476298af92b67f1f8b6a009d64ec
realaravinth <realaravinth@batsense.net>
2022-02-22 04:54:44 +0000

    Deploying to gh-pages from  @ 2919611e00e4e57e39a26c6d25d278218dedd673 🚀

--- gitpad/pages/sidebar-items.js
+++ gitpad/pages/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["context",""],["init",""],["services",""]],"mod":[["auth",""],["routes",""]],"struct":[["Footer",""],["TEMPLATES",""]]});
+initSidebarItems({"constant":[["PAYLOAD_KEY",""]],"fn":[["context",""],["init",""],["services",""]],"mod":[["auth",""],["errors",""],["routes",""]],"struct":[["Footer",""],["TEMPLATES",""],["home",""]]});
