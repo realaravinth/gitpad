70a525bba53d0d32d0714e11708e98092979c7b9
realaravinth <realaravinth@batsense.net>
2022-02-28 07:13:02 +0000

    Deploying to gh-pages from  @ 2db966c04a34586bebb88efbddedb8a20a8191bb 🚀

--- gitpad/api/v1/gists/sidebar-items.js
+++ gitpad/api/v1/gists/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["services",""]],"struct":[["CreateGistRequest",""],["CreateGistResp",""],["PostCommentRequest",""],["PostCommentResp",""],["delete_comment",""],["get_comment",""],["get_file",""],["get_gist_comments",""],["new",""],["post_comment",""]]});
+initSidebarItems({"fn":[["services",""]],"struct":[["CreateGistRequest",""],["CreateGistResp",""],["PostCommentRequest",""],["PostCommentResp",""],["delete_comment",""],["get_comment",""],["get_file",""],["get_gist_comments",""],["index",""],["new",""],["post_comment",""]]});
